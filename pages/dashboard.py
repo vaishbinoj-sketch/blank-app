a445749900d1fe2f38928b8d425ef13e7cab33b1
vaishbinoj-sketch <vaishnavvibinoj@gmail.com>
2026-09-05 01:55:57 +0400

    Update dashboard.py

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -382,7 +382,6 @@ elif selected == "Announcements":
                     st.session_state["joined"] = True
                     st.rerun()
 # ---------- PROFILE ----------
-import os
 elif selected == "My Profile":
     st.header("👤 My Profile")
     if st.session_state.get("logged_in", False):
